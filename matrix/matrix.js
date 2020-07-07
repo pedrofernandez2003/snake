fe072b195f7b6ba5f7c7b76417d6995cea38ac22
pedrofernandez2003 <pedro.fernandez2003@gmail.com>
2020-07-07 17:03:59 -0300

    agrego el movimiento del jugador y la generacion automatica de balas

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,16 +1,23 @@
 var idBala = 0;
-const velocidadBalas = 1000;
+var velocidadBalas = 1000;
+var intervaloDisparoBala = 10000;
+var balasVolando = [];
+var posJugador = 9; //esto es para tener mas a mano la posicion del jugador (aunque la puedo calcular)
 
+var dispararBalas = setInterval(generarBalaRandom, intervaloDisparoBala);
 var moverBalas = setInterval(movimientoBala, velocidadBalas);
 
 function generarMapa() {  
 	var sentenciasHTML = "<div id='parte-balas'>";
+	var jugadorHTML = "<div id='jugador'></div>";
 	for (var i = 0; i < 20; i++) {
 		var idCamino = "camino" + i;
-		sentenciasHTML = sentenciasHTML.concat("<div class='camino-bala' id='" + idCamino + "'></div>");
+		
+		sentenciasHTML = sentenciasHTML.concat("<div class='camino-bala' id='" + idCamino + "'> <div class='jugador' id='jugador" + i + "'  ></div>  </div>");
 	}
 	sentenciasHTML = sentenciasHTML.concat("</div>");
 	$("#principal").append(sentenciasHTML);
+	$("#jugador9").css('display','inline-block');
 }
 function generarBalaRandom() {
 	var posRandom =  Math.floor(Math.random() * 21);
@@ -20,17 +27,30 @@ function generarBalaRandom() {
 	console.log(idBala);
 }
 function movimientoBala(){//la linea 20 y 21 no se si son necesarias pero no las toco x si acaso, ty stackOverflow
-	var element = document.getElementById('bala1'),//esto consigue el margen right
+	var element = document.getElementById('bala0'),//esto consigue el margen right
     style = window.getComputedStyle(element),
     margenDerecho = style.getPropertyValue('margin-right');//hacer esto util por bala
 
     margenDerecho = Number(obtenerSoloNumero(margenDerecho)) + 1;
 	
-	$(".bala").css('margin-right',margenDerecho + 'px');
+	$("#bala0").css('margin-right',margenDerecho + 'px');
 }
 function obtenerSoloNumero(cadena){// le saco el px al parametro
-	var soloNumero = "";
+	var soloNumero = "";// esto se puede reemplazar por un cadena.slice(0, cadena.length -2) o substr q hace lo mismo
 	for (var i = 0; i < cadena.length && cadena[i] != 'p' ; i++) {
 		soloNumero = soloNumero + cadena[i];
 	}
-	return soloNumero;}
+	return soloNumero;
+}
+function moverJugador(donde) {
+	if (donde == "arriba" && posJugador > 0) {
+		$("#jugador" + posJugador).css('display','none');
+		posJugador --;
+		$("#jugador" + posJugador).css('display','inline-block');
+	}
+	else if (donde == "abajo" && posJugador < 19) {
+		$("#jugador" + posJugador).css('display','none');
+		posJugador ++;
+		$("#jugador" + posJugador).css('display','inline-block');
+	}
+}
