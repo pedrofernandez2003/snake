ac26643c990dc7d1e86da0d962da112d28a757c0
pedrofernandez2003 <pedro.fernandez2003@gmail.com>
2020-07-08 19:26:11 -0300

    Añado el juego casi terminado, solo hay que tratar el error de mas de una bala en la misma fila

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,11 +1,12 @@
 var idBala = 0;
-var velocidadBalas = 1000;
-var intervaloDisparoBala = 10000;
-var balasVolando = [];
+var velocidadBalas = 10;
+var intervaloDisparoBala = 1000;
+var balasVolando = {};//un diccionario q guarda el idBala y su fila
 var posJugador = 9; //esto es para tener mas a mano la posicion del jugador (aunque la puedo calcular)
-
-var dispararBalas = setInterval(generarBalaRandom, intervaloDisparoBala);
-var moverBalas = setInterval(movimientoBala, velocidadBalas);
+var anchoMatrix = 0;// la uso para calcular el margen porcentual de las balas;
+var puntuacion = 0;
+var dispararBalas;
+var verificarEstadoPartida;
 
 function generarMapa() {  
 	var sentenciasHTML = "<div id='parte-balas'>";
@@ -23,17 +24,19 @@ function generarBalaRandom() {
 	var posRandom =  Math.floor(Math.random() * 21);
 	//console.log(posRandom);
 	$("#camino" + posRandom).append("<div class='bala' id='bala" + idBala + "'><div>");
+
+	balasVolando['bala' + idBala] = posRandom;
+
 	idBala++;
-	console.log(idBala);
 }
-function movimientoBala(){//la linea 20 y 21 no se si son necesarias pero no las toco x si acaso, ty stackOverflow
-	var element = document.getElementById('bala0'),//esto consigue el margen right
+function movimientoBala(balaActual){//la linea 20 y 21 no se si son necesarias pero no las toco x si acaso, ty stackOverflow
+	var element = document.getElementById(balaActual),//esto consigue el margen right
     style = window.getComputedStyle(element),
     margenDerecho = style.getPropertyValue('margin-right');//hacer esto util por bala
 
     margenDerecho = Number(obtenerSoloNumero(margenDerecho)) + 1;
 	
-	$("#bala0").css('margin-right',margenDerecho + 'px');
+	$('#'+balaActual).css('margin-right',margenDerecho + 'px');
 }
 function obtenerSoloNumero(cadena){// le saco el px al parametro
 	var soloNumero = "";// esto se puede reemplazar por un cadena.slice(0, cadena.length -2) o substr q hace lo mismo
@@ -54,3 +57,65 @@ function moverJugador(donde) {
 		$("#jugador" + posJugador).css('display','inline-block');
 	}
 }
+function arrancarJuego() {
+	generarBalaRandom();
+	anchoMatrix = hallarAnchoMatrix();
+	dispararBalas = setInterval(generarBalaRandom, intervaloDisparoBala);
+	verificarEstadoPartida = setInterval(estadoPartida, velocidadBalas);//le pongo velocidad balas, xq quiero ver su estado por cada movimiento
+}
+function estadoPartida() {
+	for(var id in balasVolando){
+		if (obtenerDisplay(id) != "none") {
+			movimientoBala(id);
+			perder(id, balasVolando[id]);
+		}
+	}
+}
+function obtenerDisplay(balaActual) {//como no se como sacar valores del diccionario, los hago display none y su display para operar con ellos
+	var element = document.getElementById(balaActual),//esto consigue el margen right
+    style = window.getComputedStyle(element),
+    display = style.getPropertyValue('display');
+ 
+    return display;
+}
+function aumentarPuntaje(balaActual) {
+	var element = document.getElementById(balaActual),//esto consigue el margen right
+    style = window.getComputedStyle(element),
+    margenDerecho = style.getPropertyValue('margin-right');
+
+    margenDerecho = obtenerSoloNumero(margenDerecho);
+
+    margenDerechoPorc = (margenDerecho * 100) / anchoMatrix;
+	
+	if(margenDerechoPorc >= 95){
+		puntuacion++;
+		document.getElementById('puntos').innerHTML='PUNTUACION: '+ puntuacion;
+		$('#'+balaActual).css('display','none');
+	}
+}
+function perder(balaActual, fila) {//recibe una bala y se fija si esta al 90% de la matriz y si el jugador esta en la misma fila, osea si hay colision
+	var element = document.getElementById(balaActual),//esto consigue el margen right
+    style = window.getComputedStyle(element),
+    margenDerecho = style.getPropertyValue('margin-right');
+
+    margenDerecho = obtenerSoloNumero(margenDerecho);
+
+    margenDerechoPorc = (margenDerecho * 100) / anchoMatrix;
+	
+	if(margenDerechoPorc >= 85 && posJugador == fila){//pongo 85 xq si bien el jugador esta a 90, el ancho de la bala es de 5%
+		$('#'+balaActual).css('display','none');
+		document.getElementById('puntos').innerHTML='PERDISTE, PUNTOS:' + puntuacion;
+		clearInterval(dispararBalas);
+		clearInterval(verificarEstadoPartida);
+	}
+	else{
+		aumentarPuntaje(balaActual);
+	}
+}
+function hallarAnchoMatrix() {
+	var element = document.getElementById('principal'),
+    style = window.getComputedStyle(element),
+    ancho = style.getPropertyValue('width');
+
+    ancho = obtenerSoloNumero(ancho);
+    return ancho;}
